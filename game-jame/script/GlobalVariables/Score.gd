2c437b2ae72ad6ac1d87754fc9091283d38778ad
jeremleboug <106926412+jeremleboug@users.noreply.github.com>
2024-12-23 14:38:56 +0100

    Delete game-jame directory

--- game-jame/script/GlobalVariables/Score.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-var score_player1 : int = 0
-var score_player2 : int = 0
-var gagnant= "" #player1 ou player2
--- game-jame/script/transition/transition_screen.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-
-func transition(target: String) -> void:
-	$AnimationPlayer.play("default")
-	await $AnimationPlayer.animation_finished
-	get_tree().change_scene_to_file(target)
-	$AnimationPlayer.play_backwards("default")
-	

1da812557119f3f5113f7f93d1b4be6e20803747
Felipe <amanciofzani@gmail.com>
2025-01-25 15:32:07 +0100

    adding soundEffect to menus

--- devFile/game-jame/script/menu pause/menu_pause.gd
+++ devFile/game-jame/script/menu pause/menu_pause.gd
@@ -1,5 +1,6 @@
 extends Control
-
+@onready var audio_selection = $audio_selection as AudioStreamPlayer2D
+@onready var audio_survolement = $audio_survolement as AudioStreamPlayer2D
 @onready var pause_menu = $"."
 
 func _ready() -> void:
@@ -12,9 +13,16 @@ func resume():
 func _process(delta: float) -> void:
 	pass
 func _on_btn_continuer_pressed() -> void:
+	audio_selection.play()
+	await get_tree().create_timer(0.3).timeout
 	resume()
 	pass # Replace with function body.
 func _on_btn_quitter_pressed() -> void:
+	audio_selection.play()
 	get_tree().paused = false
 	TransitionScreen.transition("res://maps/menuprincipal.tscn")
-	#get_tree().change_scene_to_file("res://maps/menuprincipal.tscn")
+
+func _on_btn_continuer_mouse_entered() -> void:
+	audio_survolement.play()
+func _on_btn_quitter_mouse_entered() -> void:
+	audio_survolement.play()
--- devFile/game-jame/script/menuprincipal.gd
+++ devFile/game-jame/script/menuprincipal.gd
@@ -1,5 +1,10 @@
 class_name Menu_Principal
 extends Control
+
+@onready var audio_selection = $audio_selection
+@onready var audio_survolement = $audio_survolement
+
+
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/Button as Button #charge les assets
 @onready var exit_button = $MarginContainer/HBoxContainer/VBoxContainer/Button2 as Button
 @export var start_level = preload("res://maps/toupie.tscn") as PackedScene
@@ -10,12 +15,26 @@ func _ready() -> void:
 	
 
 func on_start_pressed():
+	audio_selection.play()
 	TransitionScreen.transition("res://maps/toupie.tscn")
 	
 func on_exit_pressed():
+	audio_selection.play()
+	await get_tree().create_timer(0.4).timeout
 	get_tree().quit()
 
-
 func _on_btn_skin_pressed() -> void:
+	audio_selection.play()
 	TransitionScreen.transition("res://maps/menus/menu_skin/menu_skin.tscn")
-	pass # Replace with function body.
+
+func _on_button_mouse_entered() -> void:
+	audio_survolement.play()
+
+func _on_btn_skin_mouse_entered() -> void:
+	audio_survolement.play()
+
+func _on_btn_skin_2_mouse_entered() -> void:
+	audio_survolement.play()
+
+func _on_button_2_mouse_entered() -> void:
+	audio_survolement.play()
